42297bc086c03aa58f0cd6c5a596c17e399ce612
akshatk806 <akshatk806@gmail.com>
2023-08-12 23:46:17 +0530

    12. 003 another way of bundled the App component in the provider

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -7,5 +7,15 @@ const ThemeContext = createContext();     // return a new context object and it 
 // <ThemeContext.Provider > -> this is the provider component that would wraps any parts of component tree to provide it with the value of this context 
 
 
+// ThemeProiver is just a custom react component, and this componenet has a template in which we render the ThemeContext provider component and inside that theme contect provider we pass any children component that this ThemeProvider component wraps and that means all the children component get access to the ThemeProvider context value 
+// any component in the application could access the value provided as prop in line 13 below by our Theme context provider
+export function ThemeProvider({ children }) {
+    // we gonna return a template, and the template is context provider component that just wraps our childer
+    return (
+        <ThemeContext.Provider value={{ color:"blue"} }>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
 
 export default ThemeContext
--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ThemeContext from './context/ThemeContext';
+import { ThemeProvider } from './context/ThemeContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* this will provide the global context value to the entire application */}
-    <ThemeContext.Provider value={{ color: 'blue' }}>    {/* value prop here, and now any component in application can access theme context */}
+    {/* this will provide the global context value to the entire application
+    <ThemeContext.Provider value={{ color: 'blue' }}>    value prop here, and now any component in application can access theme context
       <App />
-    </ ThemeContext.Provider>
+    </ ThemeContext.Provider> */}
+
+    {/* another way is */}
+    <ThemeProvider>
+      <App/>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
